40cec69858d30d0085bbf8ca2e37225e65899588
Vladyslav Tymoshenko <vlados230761@gmail.com>
2023-04-13 10:59:04 +0300

    init project, added ApiClient

--- src/components/GetProduct/GetProduct.tsx
+++ src/components/GetProduct/GetProduct.tsx
@@ -3,18 +3,22 @@ import ApiClient from "../ApiClient"
 import {useState,useEffect} from 'react'
 function GetProduct(){
     const [products, setProducts] = useState([])
-
+    const [error, setError] = useState('');
     const apiClient = new ApiClient
 
     useEffect(() => {
         apiClient
         .fetch('/products')
         .then((response) => response.json())
-        .then()
+        .then((product) => setProducts(product))
+        .catch((error) => console.log(error.message))
     },[])
-
+    if (error) {
+        return <span className="error__text">Error: {error}</span>;
+      }
     return(
-        <></>
+        <>
+        </>
     )
 }
 export default GetProduct
